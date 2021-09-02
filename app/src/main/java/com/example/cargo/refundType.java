c55bacfedfa5e8349e25946aed7de638bc5ca0fb
Aukse Matulaite <auks0017@edu.easj.dk>
2021-09-02 10:39:38 +0200

    Refund type guide added

--- app/src/main/java/com/example/cargo/refundType.java
+++ app/src/main/java/com/example/cargo/refundType.java
@@ -7,58 +7,40 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link refundType#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class refundType extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public refundType() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment refundType.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static refundType newInstance(String param1, String param2) {
-        refundType fragment = new refundType();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_refund_type, container, false);
+// Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_refund_type, container, false);
+
+        TextView text1 = view.findViewById(R.id.textField1);
+        TextView text2 = view.findViewById(R.id.textField2);
+        TextView text3 = view.findViewById(R.id.textField3);
+        TextView text4 = view.findViewById(R.id.textField4);
+        TextView text5 = view.findViewById(R.id.textField5);
+
+        text1.setText("See the refund amounts for various types of bottles and cans.\n" + "The backbone of the Danish deposit and return system is the ABC deposit system, because different amounts are refunded for ‘Pant A’, ‘Pant B’ and ‘Pant C’. ");
+        text2.setText("The amount refunded depends on the type of material used in the bottles and cans, the volume of each bottle or can and whether the bottle or can will be recycled or reused.");
+        text3.setText("> Pant A =DKK 1.00 (glass bottles and aluminium cans less than 1 litre) \n" +
+                "> Pant B = DKK 1.50 (plastic bottles less than 1 litre) \n" +
+                "> Pant C = DKK 3.00 (all bottles and cans of 1–20 litres) ");
+        text4.setText("Bottles without a deposit mark are refillable packaging that can be cleaned and refilled: for example, the green beer bottle. The deposit you pay on refillable packaging at the point of sale is refundable as follows: \n");
+        text5.setText("> Glass bottles under 0.5 litres = DKK 1.00 \n" +
+                "> Glass bottles over 0.5 litres = DKK 3.00 \n" +
+                "> Plastic bottles under 1 litre = DKK 1.50 \n" +
+                "> Plastic bottles over 1 litre = DKK 3.00 ");
+        return view;
+    }
+
+    @Override
+    public void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
     }
 }
