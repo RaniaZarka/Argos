2e6045fb4bd480794fb0f40ba0eed76e8ce5c7fc
Aukse Matulaite <auks0017@edu.easj.dk>
2021-09-28 12:13:06 +0200

    RefundType page redesigned

--- app/src/main/java/com/example/cargo/refundType.java
+++ app/src/main/java/com/example/cargo/refundType.java
@@ -1,12 +1,15 @@
 package com.example.cargo;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class refundType extends Fragment {
@@ -20,24 +23,31 @@ public class refundType extends Fragment {
 // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_refund_type, container, false);
 
+        Button button1 = view.findViewById(R.id.buttonField1);
         TextView text1 = view.findViewById(R.id.textField1);
         TextView text2 = view.findViewById(R.id.textField2);
         TextView text3 = view.findViewById(R.id.textField3);
         TextView text4 = view.findViewById(R.id.textField4);
         TextView text5 = view.findViewById(R.id.textField5);
+        TextView text6 = view.findViewById(R.id.textField6);
 
-        text1.setText("See the refund amounts for various types of bottles and cans.\n" + "The backbone of the Danish deposit and return system is the ABC deposit system, because different amounts are refunded for ‘Pant A’, ‘Pant B’ and ‘Pant C’. ");
-        text2.setText("The amount refunded depends on the type of material used in the bottles and cans, the volume of each bottle or can and whether the bottle or can will be recycled or reused.");
-        text3.setText("> Pant A =DKK 1.00 (glass bottles and aluminium cans less than 1 litre) \n" +
-                "> Pant B = DKK 1.50 (plastic bottles less than 1 litre) \n" +
-                "> Pant C = DKK 3.00 (all bottles and cans of 1–20 litres) ");
-        text4.setText("Bottles without a deposit mark are refillable packaging that can be cleaned and refilled: for example, the green beer bottle. The deposit you pay on refillable packaging at the point of sale is refundable as follows: \n");
-        text5.setText("> Glass bottles under 0.5 litres = DKK 1.00 \n" +
-                "> Glass bottles over 0.5 litres = DKK 3.00 \n" +
-                "> Plastic bottles under 1 litre = DKK 1.50 \n" +
-                "> Plastic bottles over 1 litre = DKK 3.00 ");
+
+        button1.setOnClickListener(OnClickBack);
+        text1.setText("The backbone of the Danish deposit and return system is the ABC deposit system, because different amounts are refunded for Pant A, Pant B and Pant C.");
+        text2.setText("DKK 1 \n" + "Glass bottles and aliuminium cans less than 1 litre");
+        text3.setText("DKK 1.50 \n" + "Plastic bottles less than 1 litre");
+        text4.setText("DKK 3.00 \n" + "All bottles and cans of 1-2 litres");
+        text5.setText("The amount refunded depends on the type of material used in the bottles and cans, the volume of each bottle or can and whether the bottle or can will be recycled or reused");
+        text6.setText("For more information visit us on www.website.com");
         return view;
     }
+
+    View.OnClickListener OnClickBack = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Navigation.findNavController(requireView()).navigate(R.id.fragment_argos);
+        }
+    };
 
     @Override
     public void onCreate (Bundle savedInstanceState){
