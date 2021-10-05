2933798680708d212ba4574db6ad32126bec6a67
Aukse Matulaite <auks0017@edu.easj.dk>
2021-10-05 10:00:50 +0200

    RefundType page arrow added and type 1 page started

--- app/src/main/java/com/example/cargo/type1_fragment.java
+++ app/src/main/java/com/example/cargo/type1_fragment.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.cargo.classes.PlasticType;
@@ -16,35 +17,16 @@ import com.example.cargo.classes.PlasticTypeList;
 public class type1_fragment extends Fragment {
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_type1, container, false);
+        View view = inflater.inflate(R.layout.fragment_type1, container, false);
+
+
+        return view;
     }
 
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        /*int plasticTypeNumber = plasticTypesArgs.fromBundle(getArguments()).getPlasticType();
-        PlasticType plasticType = null;
-        try {
-            plasticType = new PlasticTypeList().GetPlasticType(plasticTypeNumber);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        TextView descriptionTW = (TextView) view.findViewById(R.id.plasticDescriptionTextView);
-        TextView foundInTW = (TextView) view.findViewById(R.id.foundInTextView);
-        TextView tipsToAvoidTW = (TextView) view.findViewById(R.id.tipsToAvoidTextView);
-        TextView ratingTW = (TextView) view.findViewById(R.id.ratingTextView);
-
-        descriptionTW.setText(plasticType.getDescription());
-        foundInTW.setText(plasticType.getFoundIn());
-        tipsToAvoidTW.setText(plasticType.getTipsToAvoid());
-        ratingTW.setText(plasticType.getRating());*/
-    }
+    @Override
+    public void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState); }
 }
