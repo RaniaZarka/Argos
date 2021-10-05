45d10364145ddf1a277f7572e74e512778c3883c
RaniaZarka <raniazarka@hotmail.com>
2021-10-05 11:37:10 +0200

    added dependencies to gradle

---
+++ app/src/main/java/com/example/cargo/classes/products.java
@@ -0,0 +1,19 @@
+package com.example.cargo.classes;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class products {
+
+public String name;
+
+
+// name. picture, type, smiley
+ //Map<String, Object> item = new HashMap<>();
+
+ //item.put("name", "Pepsi 1 Liter");
+
+
+}
--- app/src/main/java/com/example/cargo/search_fragment.java
+++ app/src/main/java/com/example/cargo/search_fragment.java
@@ -8,52 +8,32 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link search_fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class search_fragment extends Fragment {
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class search_fragment<StorageReference> extends Fragment {
+
+
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    FirebaseStorage storage = FirebaseStorage.getInstance();
+    // Create a storage reference from our app
+    StorageReference storageRef = (StorageReference) storage.getReference();
+
+
 
     public search_fragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment search_fragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static search_fragment newInstance(String param1, String param2) {
-        search_fragment fragment = new search_fragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
-    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
